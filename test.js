a67c9d68334c1ebed5ca00bd2dcd2e82926f6471
KaatWillems <kaat_willems@hotmail.com>
2022-01-17 09:48:48 +0100

    commit changes

---
+++ test.js
@@ -0,0 +1,17 @@
+console.log('Hello'); 
+
+var life = 100;
+life = life - 10;
+
+var name = "Kaat" + " Willems"
+
+
+var checkout = false;
+
+var box;
+
+console.log(name);
+
+var price =a *1,21
+var a = 100 
+console.log(price)
